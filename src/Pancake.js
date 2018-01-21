af9e828c047d3672126b45727822ac84acf3366c
Steven Lundy <steven.lundy@gmail.com>
2018-01-21 08:32:02 -0800

    Add pancake functional component

---
+++ src/Pancake.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Pancake.css';
+
+const MIN_PANCAKE_SIZE = 50;
+
+function Pancake(props) {
+  const style = {
+    width: props.size * 10 + MIN_PANCAKE_SIZE
+  };
+
+  return (
+    <div
+      className={'pancake ' + (props.isBurnt ? 'is-burnt' : '')}
+      style={style}
+    />
+  );
+}
+
+export default Pancake;
