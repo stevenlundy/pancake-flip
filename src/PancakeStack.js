d6905957a166ce8c6f8f107cb34e5c3ba668eb64
Steven Lundy <steven.lundy@gmail.com>
2018-01-21 08:50:29 -0800

    Make PancakeStack functional component

---
+++ src/PancakeStack.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Pancake from './Pancake';
+
+import './PancakeStack.css';
+
+function PancakeStack(props) {
+  return (
+    <div className="pancake-stack">
+      {props.pancakes.map((pancake, i) =>
+        (<Pancake
+          key={i}
+          size={getPancakeSize(pancake)}
+          isBurnt={isPancakeBurnt(pancake)}
+        />)
+      )}
+    </div>
+  );
+}
+
+function isPancakeBurnt(pancake) {
+  return pancake < 0;
+}
+
+function getPancakeSize(pancake) {
+  return Math.abs(pancake);
+}
+
+export default PancakeStack;
